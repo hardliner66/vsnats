49f9b8647d03a54b08d4f88c1dada4db3d2e271d
hardliner66 <hardliner66@gmail.com>
2022-08-03 23:11:56 +0200

    more events

--- src/NatsSystem.cs
+++ src/NatsSystem.cs
@@ -3,6 +3,8 @@ using VSNats.Config;
 using Vintagestory.API.Datastructures;
 using Vintagestory.API.Common;
 using Vintagestory.API.Server;
+using Vintagestory.API.MathTools;
+using Vintagestory.API.Common.Entities;
 using NATS.Client;
 
 [assembly: ModInfo("VSNats",
@@ -22,32 +24,17 @@ namespace VSNats
     {
     }
 
-    abstract class PlayerEvent : NatsEvent
+    class PlayerJoinEvent : NatsEvent
     {
-        public string PlayerName { get; private set; }
-        public PlayerEvent(string player_name)
-        {
-            PlayerName = player_name;
-        }
     }
 
-    class PlayerJoinEvent : PlayerEvent
+    class PlayerDisconnectEvent : NatsEvent
     {
-        public PlayerJoinEvent(string player_name) : base(player_name)
-        {
-        }
     }
 
-    class PlayerDisconnectEvent : PlayerEvent
+    class PlayerChatEvent : NatsEvent
     {
-        public PlayerDisconnectEvent(string player_name) : base(player_name)
-        {
-        }
-    }
-
-    class PlayerChatEvent : PlayerEvent
-    {
-        public PlayerChatEvent(string player_name, int channel_id, string message, string data) : base(player_name)
+        public PlayerChatEvent(int channel_id, string message, string data)
         {
             ChannelId = channel_id;
             Message = message;
@@ -59,10 +46,136 @@ namespace VSNats
         public string Data { get; private set; }
     }
 
+    class PlayerCreateEvent : NatsEvent { }
+
+    class PlayerDeathEvent : NatsEvent
+    {
+        public PlayerDeathEvent(DamageSource damageSource)
+        {
+            DamageSource = damageSource;
+        }
+
+        public DamageSource DamageSource { get; private set; }
+    }
+
+    class PlayerLeaveEvent : NatsEvent { }
+    class PlayerNowPlayingEvent : NatsEvent { }
+    class PlayerRespawnEvent : NatsEvent { }
+    class PlayerSwitchGameModeEvent : NatsEvent
+    {
+        public PlayerSwitchGameModeEvent(EnumGameMode gameMode)
+        {
+            GameMode = gameMode;
+        }
+        public EnumGameMode GameMode { get; private set; }
+    }
+
+    class OnPlayerInteractEntityEvent : NatsEvent
+    {
+        public OnPlayerInteractEntityEvent(Entity entity, ItemSlot slot, Vec3d hitPosition, int mode)
+        {
+            Entity = entity;
+            Slot = slot;
+            HitPosition = hitPosition;
+            Mode = mode;
+        }
+        public Entity Entity { get; private set; }
+        public ItemSlot Slot { get; private set; }
+        public Vec3d HitPosition { get; private set; }
+        public int Mode { get; private set; }
+    }
+
+    class BeforeActiveSlotChangedEvent : NatsEvent
+    {
+        public BeforeActiveSlotChangedEvent(ActiveSlotChangeEventArgs eventArgs)
+        {
+            EventArgs = eventArgs;
+        }
+        public ActiveSlotChangeEventArgs EventArgs { get; private set; }
+    }
+
+    class AfterActiveSlotChangedEvent : NatsEvent
+    {
+        public AfterActiveSlotChangedEvent(ActiveSlotChangeEventArgs eventArgs)
+        {
+            EventArgs = eventArgs;
+        }
+        public ActiveSlotChangeEventArgs EventArgs { get; private set; }
+    }
+
+    class SaveGameLoadedEvent : NatsEvent { }
+
+    class SaveGameCreatedEvent : NatsEvent { }
+
+    class GameWorldSaveEvent : NatsEvent { }
+
+    class ServerSuspendEvent : NatsEvent { }
+
+    class ServerResumeEvent : NatsEvent { }
+
+    class DidPlaceBlockEvent : NatsEvent {
+        public DidPlaceBlockEvent(int oldblockId, BlockSelection blockSelection, ItemStack withItemStack) {
+            OldblockId = oldblockId;
+            BlockSelection = blockSelection;
+            WithItemStack = withItemStack;
+        }
+        public int OldblockId { get; private set; }
+        public BlockSelection BlockSelection { get; private set; }
+        public ItemStack WithItemStack { get; private set; }
+    }
+
+    class CanPlaceOrBreakBlockEvent : NatsEvent {
+        public CanPlaceOrBreakBlockEvent(BlockSelection blockSelection) {
+            BlockSelection = blockSelection;
+        }
+        public BlockSelection BlockSelection { get; private set; }
+    }
+
+    class BreakBlockEvent : NatsEvent {
+        public BreakBlockEvent(BlockSelection blockSelection, float dropQuantityMultiplier) {
+            BlockSelection = blockSelection;
+            DropQuantityMultiplier = dropQuantityMultiplier;
+        }
+        public BlockSelection BlockSelection { get; private set; }
+        public float DropQuantityMultiplier { get; private set; }
+    }
+
+    class DidBreakBlockEvent : NatsEvent
+    {
+        public DidBreakBlockEvent(int oldblockId, BlockSelection blockSelection)
+        {
+            OldblockId = oldblockId;
+            BlockSelection = blockSelection;
+        }
+        public int OldblockId { get; private set; }
+        public BlockSelection BlockSelection { get; private set; }
+    }
+
+    class DidUseBlockEvent : NatsEvent
+    {
+        public DidUseBlockEvent(BlockSelection blockSelection)
+        {
+            BlockSelection = blockSelection;
+        }
+        public BlockSelection BlockSelection { get; private set; }
+    }
+
+    class ChunkColumnLoadedEvent : NatsEvent { }
+
+    class ChunkColumnUnloadedEvent : NatsEvent { }
+
+    class MapRegionLoadedEvent : NatsEvent { }
+
+    class ChunkColumnSnowUpdateEvent : NatsEvent { }
+
+    class OnTrySpawnEntityEvent : NatsEvent { }
+
+    class MapRegionUnloadedEvent : NatsEvent { }
+
     /// <summary> Main system for the "VSNats" mod. </summary>
     public class NatsSystem : ModSystem
     {
-        public static string MOD_ID = "vsnats";
+        public static string MOD_ID = "VSNats";
 
         public override bool AllowRuntimeReload => true;
 
@@ -79,7 +192,8 @@ namespace VSNats
         public override void StartServerSide(ICoreServerAPI api)
         {
             ServerAPI = api;
-            var config = api.LoadModConfig<NatsConfig>("nats.json");
+            var config_name = $"{MOD_ID}.json";
+            var config = api.LoadModConfig<NatsConfig>(config_name);
             bool dirty = false;
             if (config == null)
             {
@@ -107,33 +221,150 @@ namespace VSNats
 
             if (dirty)
             {
-                api.StoreModConfig(config, "nats.json");
+                api.StoreModConfig(config, config_name);
             }
 
             Options opts = ConnectionFactory.GetDefaultOptions();
             opts.Url = config.Url;
             nats = new ConnectionFactory().CreateConnection(opts);
 
-            string GLOBAL_EVENTS = $"{config.NatsPrefix}.{config.ServerId}";
-            string PLAYER_EVENTS = $"{GLOBAL_EVENTS}.player";
+            string GLOBAL_EVENTS = $"{config.NatsPrefix}.server.{config.ServerId}";
+            var GetPlayerSubject = (IPlayer player) => $"{GLOBAL_EVENTS}.player.{player.PlayerName}";
 
             nats.PublishTyped(GLOBAL_EVENTS, new StartEvent());
-
-            api.Event.PlayerJoin += (player) =>
-            {
-                nats.PublishTyped<PlayerJoinEvent>(GLOBAL_EVENTS, new PlayerJoinEvent(player.PlayerName));
-            };
-
-            api.Event.PlayerDisconnect += (player) =>
-            {
-                nats.PublishTyped(GLOBAL_EVENTS, new PlayerDisconnectEvent(player.PlayerName));
-            };
 
             api.Event.PlayerChat += (IServerPlayer player, int channelId, ref string message, ref string data, BoolRef consumed) =>
             {
                 consumed.SetValue(false);
-                nats.PublishTyped(GLOBAL_EVENTS, new PlayerChatEvent(player.PlayerName, channelId, message, data));
+                nats.PublishTyped(GetPlayerSubject(player), new PlayerChatEvent(channelId, message, data));
             };
+
+            api.Event.PlayerCreate += (player) =>
+            {
+                nats.PublishTyped(GetPlayerSubject(player), new PlayerCreateEvent());
+            };
+
+            api.Event.PlayerDeath += (player, damageSource) =>
+            {
+                nats.PublishTyped(GetPlayerSubject(player), new PlayerDeathEvent(damageSource));
+            };
+
+            api.Event.PlayerDisconnect += (player) =>
+            {
+                nats.PublishTyped(GetPlayerSubject(player), new PlayerDisconnectEvent());
+            };
+
+            api.Event.PlayerJoin += (player) =>
+            {
+                nats.PublishTyped(GetPlayerSubject(player), new PlayerJoinEvent());
+            };
+
+            api.Event.PlayerLeave += (player) =>
+            {
+                nats.PublishTyped(GetPlayerSubject(player), new PlayerLeaveEvent());
+            };
+
+            api.Event.PlayerNowPlaying += (player) =>
+            {
+                nats.PublishTyped(GetPlayerSubject(player), new PlayerNowPlayingEvent());
+            };
+
+            api.Event.PlayerRespawn += (player) =>
+            {
+                nats.PublishTyped(GetPlayerSubject(player), new PlayerRespawnEvent());
+            };
+
+            api.Event.PlayerSwitchGameMode += (player) =>
+            {
+                nats.PublishTyped(GetPlayerSubject(player), new PlayerSwitchGameModeEvent(player.WorldData.CurrentGameMode));
+            };
+
+            api.Event.OnPlayerInteractEntity += (Entity entity, IPlayer player, ItemSlot slot, Vec3d hitPosition, int mode, ref EnumHandling handling) =>
+            {
+                handling = EnumHandling.PassThrough;
+                nats.PublishTyped(GetPlayerSubject(player), new OnPlayerInteractEntityEvent(entity, slot, hitPosition, mode));
+            };
+
+            api.Event.BeforeActiveSlotChanged += (player, ev) =>
+            {
+                nats.PublishTyped(GetPlayerSubject(player), new BeforeActiveSlotChangedEvent(ev));
+                return EnumHandling.PassThrough;
+            };
+
+            api.Event.AfterActiveSlotChanged += (player, ev) =>
+            {
+                nats.PublishTyped(GetPlayerSubject(player), new AfterActiveSlotChangedEvent(ev));
+            };
+
+            api.Event.DidBreakBlock += (player, oldblockId, blockSelection) =>
+            {
+                nats.PublishTyped(GetPlayerSubject(player), new DidBreakBlockEvent(oldblockId, blockSelection));
+            };
+
+            api.Event.DidUseBlock += (player, blockSelection) =>
+            {
+                nats.PublishTyped(GetPlayerSubject(player), new DidUseBlockEvent(blockSelection));
+            };
+
+            api.Event.SaveGameLoaded += () =>
+            {
+                nats.PublishTyped(GLOBAL_EVENTS, new SaveGameLoadedEvent());
+            };
+
+            api.Event.SaveGameCreated += () =>
+            {
+                nats.PublishTyped(GLOBAL_EVENTS, new SaveGameCreatedEvent());
+            };
+
+            api.Event.GameWorldSave += () =>
+            {
+                nats.PublishTyped(GLOBAL_EVENTS, new GameWorldSaveEvent());
+            };
+
+            api.Event.ServerSuspend += () =>
+            {
+                nats.PublishTyped(GLOBAL_EVENTS, new ServerSuspendEvent());
+                return EnumSuspendState.Ready;
+            };
+
+            api.Event.ServerResume += () =>
+            {
+                nats.PublishTyped(GLOBAL_EVENTS, new ServerResumeEvent());
+            };
+
+            api.Event.DidPlaceBlock += (player, oldblockId, blockSelection, withItemStack) => {
+                nats.PublishTyped(GetPlayerSubject(player), new DidPlaceBlockEvent(oldblockId, blockSelection, withItemStack));
+            };
+
+            // crashes the server
+            // api.Event.CanPlaceOrBreakBlock += (player, blockSelection) =>
+            // {
+            //     nats.PublishTyped(GetPlayerSubject(player), new CanPlaceOrBreakBlockEvent(blockSelection));
+            //     // todo: check if returning true makes problems
+            //     return true;
+            // };
+
+            // crashes the server
+            // api.Event.BreakBlock += (IServerPlayer player, BlockSelection blockSelection, ref float dropQuantityMultiplier, ref EnumHandling handling) =>
+            // {
+            //     nats.PublishTyped(GetPlayerSubject(player), new BreakBlockEvent(blockSelection, dropQuantityMultiplier));
+            //     handling = EnumHandling.PassThrough;
+            // };
+
+            // api.Event.ChunkColumnLoaded += (chunkCoord, chunks) => { };
+
+            // api.Event.ChunkColumnUnloaded += (chunkCoord) => { };
+
+            // api.Event.MapRegionLoaded += (mapCoord, region) => { };
+
+            // api.Event.ChunkColumnSnowUpdate += (mapChunk, chunkX, chunkZ, chunks) => { };
+
+            // api.Event.OnTrySpawnEntity += (ref EntityProperties properties, Vec3d spawnPosition, long herdId) =>
+            // {
+            //     return true;
+            // };
+
+            // api.Event.MapRegionUnloaded += (mapCoord, region) => { };
         }
     }
 }
